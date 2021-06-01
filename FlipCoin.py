7e1ca9426c7990ec20e050fb21fb54d8e2baea68
santanu619 <mohapatra.santanu123@gmail.com>
2021-06-01 17:40:54 +0530

    Added:Percentage of flips for head and tail

---
+++ FlipCoin.py
@@ -0,0 +1,28 @@
+'''
+@Author: Santanu Mohapatra
+@Date: 01/06/2021
+@Title: Flip Coin and print percentage of Heads and Tails
+'''
+import random
+head=0
+tail=0
+try: 
+    number=int(input("Enter the number of times to flip the coin:"))
+    print("HEADS or TAILS")
+except ValueError:
+    print("Please give the correct number")
+if number < 0:
+    print("Wrong Number")
+else:
+    for x in range(0,number):
+        flip=random.random()
+        if flip < 0.5:
+            tail+=1
+        else:
+            head+=1
+head_percent=(head/number)*100
+tail_percent=(tail/number)*100
+print("Number of Heads:", head)
+print("Number of Tails:", tail)
+print("Percentage of Head:", head_percent)
+print("Percentage of Tail:", tail_percent)
