e6753daff30ce977c0fff85f4f5bf0a690096fd7
santanu619 <mohapatra.santanu123@gmail.com>
2021-06-02 20:02:27 +0530

    Refactor: Adding the business logic inside the try block

--- PrimeFactorization.py
+++ PrimeFactorization.py
@@ -5,10 +5,10 @@
 '''
 try:
     number=int(input("Enter the number:"))
+    for index in range(2,number):
+        while(number%index==0):
+            print(index)
+            number=number/index
+            print(number)
 except ValueError:
     print("Please enter the correct number")
-for index in range(2,number):
-    while(number%index==0):
-        print(index)
-        number=number/index
-        print(number)
