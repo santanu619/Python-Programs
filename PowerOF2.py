cf724244f3cb871cada89306d223ca6d5b519881
santanu619 <mohapatra.santanu123@gmail.com>
2021-06-02 19:58:49 +0530

    Refactor: Adding the business logic inside the try block

--- PowerOF2.py
+++ PowerOF2.py
@@ -4,14 +4,14 @@
 @Title: Power of 2
 '''
 power=1
-N=2
+MultiplicationPower=2
 try:
     number=int(input("Enter the power value of N"))
+    if 0 < number < 31:
+        for x in range(0,number):
+            power=(power*MultiplicationPower)
+            print(power)
+    else:
+        print("Power is out of range. Give another number.")
 except ValueError:
     print("Enter the correct number")
-if 0 < number < 31:
-    for x in range(0,number):
-        power=(power*N)
-        print(power)
-else:
-    print("Power is out of range. Give another number.")
