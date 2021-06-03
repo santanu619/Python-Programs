806bd681a76dc739a113095784fe13e67cf77089
santanu619 <mohapatra.santanu123@gmail.com>
2021-06-03 11:33:32 +0530

    Refactor: Added try block inside the body of function

--- Gambler.py
+++ Gambler.py
@@ -18,25 +18,25 @@ except ValueError:
 
   
 #Define a function to run N number of times and bet stake until goal is reached or becomes zero.
-try:
     def gambler():
-        count=0
-        win=0  
-        for i in range(number_of_time):
-            bet = stake  
-            while bet > 0 and bet < goal:
-                count += 1 
-                if r.randint(0,1) == 0:
-                    bet += 1
-                else:
-                    bet -= 1
+        try:
+            count=0
+            win=0  
+            for i in range(number_of_time):
+                bet = stake  
+                while bet > 0 and bet < goal:
+                    count += 1 
+                    if r.randint(0,1) == 0:
+                        bet += 1
+                    else:
+                        bet -= 1
                
-            if bet == goal:
-                win += 1
-        print(win/number_of_time*100)
-        print(bet) 
-except NameError as e:
-    print(e)
+                if bet == goal:
+                    win += 1
+            print(win/number_of_time*100)
+            print(bet) 
+        except NameError as e:
+            print(e)
 
 """
 A function to create the gambler problem where the gambler will bet and the function will print the number of times the gambler won.
