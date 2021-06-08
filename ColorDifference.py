b48258b0747c39120b15b154a36f52f41e828366
santanu619 <mohapatra.santanu123@gmail.com>
2021-06-08 21:30:34 +0530

    Refactor: Added code to print the color from list 1 which is not present in list 2

---
+++ ColorDifference.py
@@ -0,0 +1,13 @@
+'''
+@Author: Santanu Mohapatra
+@Date: 08/06/2021
+@Last Modified by: Santanu Mohapatra
+@Last Modified Time: 18:40 PM
+@Title: To print out a set containing all the colors from color_list_1 which are not present in color_list_2
+'''
+
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+
+print(color_list_1.difference(color_list_2))
+
